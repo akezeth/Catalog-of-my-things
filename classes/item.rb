365284222f62f52697234f6409a1c1971e8feb86
akezeth <116703083+akezeth@users.noreply.github.com>
2023-08-14 16:14:57 +0300

    Add methods

--- classes/item.rb
+++ classes/item.rb
@@ -1,3 +1,5 @@
+require 'date'
+
 class Item
   attr_accessor :id, :genre, :author, :source, :label, :publish_date, :archived
 
@@ -5,18 +7,31 @@ class Item
     @id = params[:id] || rand(1..10_000)
     @genre = params[:genre]
     @author = params[:author]
-    @source = params[:source]
     @label = params[:label]
     @publish_date = params[:publish_date]
     @archived = params[:archived] || false
   end
 
+  def move_to_archive
+    @archived = true if can_be_archived?
+  end
+
+  def add_genre(genre)
+    @genre = genre
+  end
+
+  def add_author(author)
+    @author = author
+  end
+
+  def add_label(label)
+    @label = label
+  end
+
   private
 
   def can_be_archived?
-    return true if (Date.today - @publish_date).to_i / 365 > 10
-
-    false
+    (Date.today - @publish_date).to_i / 365 > 10
   end
 
   def create_date(date_str)
