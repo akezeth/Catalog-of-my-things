47d8f9730adcff7f635afe1e44b5670c366722a7
akezeth <116703083+akezeth@users.noreply.github.com>
2023-08-15 16:02:11 +0300

    Update Genre class

--- classes/genre.rb
+++ classes/genre.rb
@@ -8,6 +8,9 @@ class Genre
   end
 
   def add_item(item)
-    @items << item unless @items.include?(item)
+    return if @items.include?(item)
+
+    @items << item
+    item.add_genre(self)
   end
 end
