d006b569b74c54f741c2afeb6d293c8160d8853c
akezeth <116703083+akezeth@users.noreply.github.com>
2023-08-16 17:16:17 +0300

    Add add_music_album module

---
+++ modules/add_music_album.rb
@@ -0,0 +1,38 @@
+module AddMusicAlbum
+  def add_musicAlbum
+    publish_date = input_date
+    on_spotify = input_on_spotify
+    music_ablum = MusicAlbum.new(publish_date: publish_date, on_spotify: on_spotify)
+    @music_albums << music_ablum
+    genre = add_genre
+    music_ablum.add_genre(genre)
+    puts 'Music Album added successfully'
+  end
+
+  def input_date
+    print 'Publish date: '
+    publish_date = gets.chomp
+  end
+  
+  def input_on_spotify
+    print 'Is it on spotify [Y/N]'
+    on_spotify = gets.chomp.upcase
+    until on_spotify.eql?('Y') || on_spotify.eql?('N')
+      print 'Invalid input. Please enter the correct option [Y/N]: '
+      on_spotify = gets.chomp.upcase
+    end
+    on_spotify == 'Y'
+  end
+
+  def add_genre
+    puts 'Add Genre for the music album'
+    print 'Enter the name of Genre: '
+    name = gets.chomp
+    @genres.each do |genre|
+      if genre.name.include?(name)
+        return genre
+    new_genre = Genre.new(name: name)
+    @genres << new_genre
+    new_genre
+  end
+end
