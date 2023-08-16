4517ac409cdecf629faa5cac2745543b0efdbafa
Massam Khan <hafizmassamtabraizkhan@gmail.com>
2023-08-16 17:53:20 +0500

    Implement list_all_labels method

--- modules/book_label/book_label.rb
+++ modules/book_label/book_label.rb
@@ -9,4 +9,14 @@ module BookAndLabel
       end
     end
   end
+
+  def list_all_labels
+    if @label.empty?
+      puts 'No labels available.'
+    else
+      @label.each_with_index do |label, index|
+        puts "#{index}) Label Title: #{label.title}, Label Color: #{label.color}"
+      end
+    end
+  end
 end
