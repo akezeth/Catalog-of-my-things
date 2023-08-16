3e151b4a609eb66b99d90ca7758c1051ddc366d7
Massam Khan <hafizmassamtabraizkhan@gmail.com>
2023-08-17 00:55:03 +0500

    Add query for creating books table

--- schema.sql
+++ schema.sql
@@ -1,1 +1,11 @@
 CREATE DATABASE catalogue_of_things;
+
+CREATE TABLE books (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  publish_date DATE,
+  label_id INT REFERENCES labels(id),
+  can_be_archived BOOLEAN,
+  cover_state VARCHAR(250),
+  publisher VARCHAR(250),
+  PRIMARY KEY(id)
+);
