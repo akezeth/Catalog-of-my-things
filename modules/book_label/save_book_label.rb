f01470913b65a46bb49e5135c58ec29120897b30
Massam Khan <hafizmassamtabraizkhan@gmail.com>
2023-08-17 00:45:34 +0500

    Add method for saving a book

--- modules/book_label/save_book_label.rb
+++ modules/book_label/save_book_label.rb
@@ -4,4 +4,17 @@ module SaveBookAndLabelData
   def save_to_file(file_name, data)
     File.write(file_name, JSON.pretty_generate(data))
   end
+
+  def save_book
+    books_hash = @books.map do |book|
+      {
+        cover_state: book.cover_state,
+        publisher: book.publisher,
+        publish_date: book.publish_date,
+        id: book.id,
+        label: book.label.title
+      }
+    end
+    save_to_file('data/book.json', books_hash)
+  end
 end
